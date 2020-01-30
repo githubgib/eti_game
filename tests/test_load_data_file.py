57dfa401dfe4a93fc19cebf69cb21fa9b5c14071
Darren Yap <56539537+DarrenYapZW@users.noreply.github.com>
2020-01-30 23:26:56 +0800

    Add files via upload
    Test case for read and load maze from file requirement.

---
+++ tests/test_load_data_file.py
@@ -0,0 +1,22 @@
+import pytest
+from mazegame.maze_game_functions import *
+
+def test_data_file_blank():
+    value == " "
+    assert value == "error"
+
+def test_data_file_wrong():
+    value == "maze1.csv"
+    assert value == "error"
+
+def test_data_file_wrong_extension():
+    value == "maze.ppt"
+    assert value == "error"
+
+def test_data_file_no_extension():
+    value == "maze"
+    assert vaule == "error"
+
+def test_data_file_correct():
+    value == "maze.csv"
+    assert vaule == 8
